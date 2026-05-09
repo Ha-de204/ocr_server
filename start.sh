0d2bbbf0f8b622cf73ef0415688d9cff98d041a8
Ha Nguyen <ngha17012004@gmail.com>
2026-05-09 15:04:42 +0700

    fix

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # Cài Tesseract
-apt-get update
-apt-get install -y tesseract-ocr tesseract-ocr-vie
+#apt-get update
+#apt-get install -y tesseract-ocr tesseract-ocr-vie
 
 # Chạy server
-uvicorn main:app --host 0.0.0.0 --port 10000
+uvicorn main:app --host 0.0.0.0 --port $PORT
